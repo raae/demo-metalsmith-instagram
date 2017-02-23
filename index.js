79068ef44cefffdf928f18c3f3cdb7803f71dca0
Benedicte Raae <raae@bgraphic.no>
2017-02-23 10:39:55 +0100

    Static site with Instagram photos

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
+require('dotenv').config();
+
 var Metalsmith  = require('metalsmith');
 var markdown    = require('metalsmith-markdown');
+var remote      = require('metalsmith-remote-json-to-files')
 var layouts     = require('metalsmith-layouts');
 var permalinks  = require('metalsmith-permalinks');
 var assets      = require('metalsmith-assets');
+var collections = require('metalsmith-collections');
 
 Metalsmith(__dirname)
   .metadata({
-    title: 'Metalsmith ❤️ Instagram',
-    description: "Content from Instagram. Static site by Metalsmith. Created by Benedicte (@raae)",
-    generator: "Metalsmith",
-    url: "http://www.metalsmith.io/"
+    site: {
+      title: 'Metalsmith ❤️ Instagram',
+      description: "Content from Instagram. Static site by Metalsmith. Created by Benedicte (@raae)",
+      social: {
+        twitter: 'https://twitter.com/raae',
+        github: 'https://github.com/raae'
+      }
+    }
   })
   .source('./src')
   .destination('./build')
   .clean(true)
+  .use(remote({
+    url: 'https://api.instagram.com/v1/users/self/media/recent/?access_token=' + process.env.IG_ACCESS_TOKEN,
+    transformOpts: function(json) {
+      return json.data.reduce((prev, item) => {
+        return Object.assign(prev, {
+          ['images/'+ item.id + '.md']: {
+            slug: item.id,
+            date: new Date(parseInt(item.created_time) * 1000).toISOString(),
+            caption: item.caption.text,
+            src: item.images,
+            contents: ''
+          }
+        })
+    }, {})
+    }
+  }))
+  .use(collections({
+    images: {
+      pattern: 'images/*',
+      sortBy: 'date',
+      reverse: true
+    }
+  }))
   .use(markdown())
   .use(permalinks())
   .use(layouts({
--- src/index.md
+++ src/index.md
@@ -1,3 +1,11 @@
 ---
+title: Demo by Benedicte
 layout: layout.html
 ---
+
+This is a statically generated site with images from my personal Instagram account.
+
+There is no front-end code getting these photos. They are fetched when
+generating the static site using Metalsmith.
+
+If you would like to know how check out the tutorial on [bgraphic.no](bgraphic.no).
